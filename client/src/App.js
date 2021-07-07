bdfba5ca60a0985f89cc0339dcafcf68e8f06cc1
Hristo Karchev <x_karchev@mail.bg>
2021-07-07 22:41:18 +0300

    logo created and added

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ import Header from './components/Header/Header';
 function App() {
   return (
     <div className="app">
-      <h1>Hey Dumbass</h1>
       <Header />
     </div>
   );
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 function Header() {
     return (
         <div className='header'>
-            <img className="header__logo" src="/as.png" alt="logo" />
+            <img className="header__logo" src="/as3.png" alt="logo" />
 
             <div className="header__search">
                 <input className="header__searchInput" type="text" />
