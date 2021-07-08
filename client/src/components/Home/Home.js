53545e18598807a788b18029058b037ef4040447
Hristo Karchev <x_karchev@mail.bg>
2021-07-08 14:18:26 +0300

    Home page image created&added + style

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Home.css"
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__container">
+                <img className="home__image" src="/home.jpg" alt="home image"/>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home
