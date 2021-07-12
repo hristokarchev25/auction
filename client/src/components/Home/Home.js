f8029730c041ce1686be99004ac0c2124bbe3454
Hristo Karchev <x_karchev@mail.bg>
2021-07-12 11:23:48 +0300

    favicon changed

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -5,7 +5,7 @@ function Home() {
     return (
         <div className="home">
             <div className="home__container">
-                <img className="home__image" src="/home.jpg" alt="home image"/>
+                <img className="home__image" src="/home.jpg" alt="home"/>
             </div>
 
         </div>
