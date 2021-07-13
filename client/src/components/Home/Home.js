994b671bcef1aee2140ebfda0377037d1c0fc4ba
Hristo Karchev <x_karchev@mail.bg>
2021-07-13 11:24:01 +0300

    css for home page and index.css

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import "./Home.css"
+
 import Product from './Product/Product';
 
 function Home() {
     return (
-        <div className="home">
+        <div className="home" >
             <div className="home__container">
                 <img className="home__image" src="/home.jpg" alt="home" />
-            </div>
-
-            <div className="home__row">
-                <Product />
+                <div className="home__row" >
+                    <Product />
+                    <Product />
+                    <Product />
+                    
+                </div>
             </div>
         </div>
     )
