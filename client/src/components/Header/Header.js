dce007101f6f811152c898834193456d8bbc63bd
Hristo Karchev <x_karchev@mail.bg>
2021-07-14 12:29:08 +0300

    hammer icon added + link to home page in header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
+import GavelIcon from '@material-ui/icons/Gavel';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <div className='header'>
-            <img className="header__logo" src="/as3.png" alt="logo" />
+            <Link to="/">
+                <img className="header__logo" src="/as3.png" alt="logo" />
+            </Link>
 
             <div className="header__search">
                 <input className="header__searchInput" type="text" />
@@ -21,9 +25,9 @@ function Header() {
                     <span className='header__optionLineOne'>Hello, Guest</span>
                     <span className='header__optionLineTwo'>Sign In</span>
                 </div>
-                <div className="header__option">
-                    <span className='header__optionLineOne'>Hello, Guest</span>
-                    <span className='header__optionLineTwo'>Sign In</span>
+                <div className="header__optionHammer">
+                    <GavelIcon />
+                    <span className='header__optionLineTwo header__hammerCount'>0</span>
                 </div>
             </div>
         </div>
