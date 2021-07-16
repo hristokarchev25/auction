73f213195c717c21049c5635eed47a708b96c782
Hristo Karchev <x_karchev@mail.bg>
2021-07-16 16:58:44 +0300

    Create form created + css

--- client/src/components/Create/Create.js
+++ client/src/components/Create/Create.js
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import './Create.css'
 function Create() {
     return (
-        <div>
-            <h1>Create bid</h1>
+        <div className="create">
+            <div className="create__container">
+                <h1>Start an auction by adding a product</h1>
+
+                <form>
+                    <h5>Name of product</h5>
+                    <input type="text" />
+
+                    <h5>Description</h5>
+                    <input type="textarea" />
+
+                    <h5>Price</h5>
+                    <input type="text" />
+
+                    <h5>Image</h5>
+                    <input type="text" />
+
+                    <button className="create__btn">Create product</button>
+                </form>
+
+            </div>
         </div>
     )
 }
