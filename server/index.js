b51552b27f8fa622fefeeebeb87c0d5f843c1808
Hristo Karchev <x_karchev@mail.bg>
2021-07-21 12:14:22 +0300

    Mongo is successfully connected!

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const app = express();
 
-/* const mongoose = require("mongoose");
+const mongoose = require("mongoose");
+const connection_url = 'mongodb+srv://admin:SbiWUjPPBYoNqo7C@realmcluster.2crnj.mongodb.net/systemdb?retryWrites=true&w=majority';
+
+/* 
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser"); */
 
-app.get("/", (req, res)=>res.status(200).send("Hello!!"));
+//db
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log("MongoDB is successfully connected!"))
+    .catch((err) => console.log(err));
 
+//api endpoints
+app.get("/", (req, res) => res.status(200).send("Hello!!"));
+
+
+//listener
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`The server is running on port: ${port}!!`);
