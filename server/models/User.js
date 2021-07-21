d341c2641d13a4c4de0222967d1f1b301dbb9baa
Hristo Karchev <x_karchev@mail.bg>
2021-07-21 22:35:52 +0300

    Salt rounds applied

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
     name: {
@@ -16,6 +19,29 @@ const userSchema = mongoose.Schema({
     },
     token: {
         type: String,
+    }
+});
+
+userSchema.pre("save", function (next) {
+    var user = this;
+
+    if (user.isModified("password")) {
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) {
+                return next(err);
+            }
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) {
+                    return next(err);
+                }
+
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
     }
 });
 
