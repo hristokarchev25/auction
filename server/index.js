f095f2da664591b5fff76733db7c34129a8d71d2
Hristo Karchev <x_karchev@mail.bg>
2021-07-21 16:24:54 +0300

    test for creating product successful

--- server/index.js
+++ server/index.js
@@ -4,10 +4,14 @@ const app = express();
 const mongoose = require("mongoose");
 const db_url = require("./config/db");
 
+const cors = require("cors");
+
+const { Product } = require("./models/Product");
 /* 
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser"); */
-
+app.use(express.json());
+app.use(cors());
 //db
 mongoose.connect(db_url.connection_url, {
     useNewUrlParser: true,
@@ -19,6 +23,31 @@ mongoose.connect(db_url.connection_url, {
 
 //api endpoints
 app.get("/", (req, res) => res.status(200).send("Hello!!"));
+
+app.post("/product/card", (req, res) => {
+    const productCard = req.body;
+
+    Product.create(productCard, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(data)
+        }
+    })
+
+});
+
+app.get("/product/card", (req, res) => {
+
+    Product.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+    })
+
+});
 
 
 //listener
