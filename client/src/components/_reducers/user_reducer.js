1a4d0033a3773f858b513110acf1436d67e1f77f
Hristo Karchev <x_karchev@mail.bg>
2021-07-24 08:39:47 +0300

    user reducer created + set up for roor reducer

---
+++ client/src/components/_reducers/user_reducer.js
@@ -0,0 +1,18 @@
+import { LOGIN_USER, REGISTER_USER, AUTH_USER, LOGOUT_USER } from "../_actions/types";
+
+const userState = (state = {}, action) => {
+    switch (action.type) {
+        case LOGIN_USER:
+            return { ...state, loginSuccess: action.payload };
+        case REGISTER_USER:
+            return { ...state, success: action.payload };
+        case AUTH_USER:
+            return { ...state, userData: action.payload };
+        case LOGOUT_USER:
+            return { ...state };
+        default:
+            return state;
+    }
+};
+
+export default userState;
