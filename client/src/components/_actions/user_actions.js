04f841ef9ecb0b6e192a9879c378ef7755562d45
Hristo Karchev <x_karchev@mail.bg>
2021-07-24 08:29:59 +0300

    auth and logout actions (get)

--- client/src/components/_actions/user_actions.js
+++ client/src/components/_actions/user_actions.js
@@ -4,24 +4,48 @@ import { LOGIN_USER, REGISTER_USER, AUTH_USER, LOGOUT_USER } from "./types";
 
 export function loginUser(dataToSubmit) {
     const request = axios
-      .post("/users/login", dataToSubmit)
-      .then((response) => response.data)
-      .catch((error) => console.error("Error:", error));
-  
-    return {
-      type: LOGIN_USER,
-      payload: request,
-    };
-  };
+        .post("/users/login", dataToSubmit)
+        .then((response) => response.data)
+        .catch((error) => console.error("Error:", error));
 
-  export function registerUser(dataToSubmit) {
-    const request = axios
-      .post(`/users/register`, dataToSubmit)
-      .then((response) => response.data)
-      .catch((error) => console.error("Error:", error));
-  
     return {
-      type: REGISTER_USER,
-      payload: request,
+        type: LOGIN_USER,
+        payload: request,
     };
-  };
+};
+
+export function registerUser(dataToSubmit) {
+    const request = axios
+        .post("/users/register", dataToSubmit)
+        .then((response) => response.data)
+        .catch((error) => console.error("Error:", error));
+
+    return {
+        type: REGISTER_USER,
+        payload: request,
+    };
+};
+
+export function auth() {
+    const request = axios
+        .get("/users/auth")
+        .then((response) => response.data)
+        .catch((error) => console.error("Error:", error));
+
+    return {
+        type: AUTH_USER,
+        payload: request,
+    };
+};
+
+export function logoutUser() {
+    const request = axios
+        .get("/users/logout")
+        .then((response) => response.data)
+        .catch((error) => console.error("Error:", error));
+
+    return {
+        type: LOGOUT_USER,
+        payload: request,
+    };
+};
