20ad4cc4236b403b849376512b9dd9a2788a86f7
Hristo Karchev <x_karchev@mail.bg>
2021-07-24 07:58:46 +0300

    redux installed + cookie parser added to server

--- server/index.js
+++ server/index.js
@@ -5,11 +5,11 @@ const mongoose = require("mongoose");
 const db_url = require("./config/db");
 
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
-
-/* const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser"); */
+/* const bodyParser = require("body-parser");*/
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
 
 //db
@@ -23,7 +23,7 @@ mongoose.connect(db_url.connection_url, {
 
 //api endpoints
 app.use("/product", require('./routes/product'));
-
+app.use("/users", require('./routes/user'));
 //listener
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
